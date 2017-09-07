4884e90d2ea86fa8a0d4d03431a3ba8fae2c86cd
Jelskipro <Jelle.hoogenberg@student.hku.nl>
2017-09-07 13:14:40 +0200

    Opdracht 1 V1.0

---
+++ opdracht1/src/ofApp.cpp
@@ -0,0 +1,53 @@
+#include "ofApp.h"
+
+//--------------------------------------------------------------
+void ofApp::setup()
+{
+	x = ofRandom(0, ofGetWidth());
+	y = ofRandom(0, ofGetHeight());
+
+	speedX = ofRandom(-3, 3);
+	speedY = ofRandom(-3, 3);
+
+	radius = 40;
+	
+}
+
+//--------------------------------------------------------------
+void ofApp::update()
+{
+	if (x < radius ) {
+		x = radius;
+		speedX = -speedX;
+	}
+	else if (x > ofGetWidth()) {
+		x = ofGetWidth();
+		speedX = -speedX;
+	}
+	if (y < radius) {
+		y = radius;
+		speedY = -speedY;
+	}
+	else if (y > ofGetHeight()) {
+		y = ofGetHeight();
+		speedY = -speedY;
+	}
+	
+	x += speedX;
+	y += speedY;
+}
+
+//--------------------------------------------------------------
+void ofApp::draw()
+{
+	ofSetColor(ofColor(ofRandom(0, 255), ofRandom(0, 255), ofRandom(0, 255)));
+	ofDrawCircle(x, y, radius);
+}
+
+//--------------------------------------------------------------
+void ofApp::keyPressed(int key)
+{
+
+}
+
+//--------------------------------------------------------------
